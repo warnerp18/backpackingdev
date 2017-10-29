4947ae2744de7e777dbb5e268b74090e42d13e97
phil <warnerp18@gmail.com>
2017-10-29 13:50:34 -0700

    move routes to app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+
+import HomePage from './home-page';
+import Nav from './nav';
+import WorkList from './work-list';
+import BlogList from './blog-list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        I'm Phil
-        Welcome to my site
+        <Nav />
+        <Route path="/work" component={WorkList} />
+        <Route path="/blog" component={BlogList} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import createBrowserHistory from 'history/createBrowserHistory';
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import Nav from './components/nav';
 import App from './components/app';
-import WorkList from './components/work-list';
-import BlogList from './components/blog-list';
 
-const customHistory = createBrowserHistory()
-
-render(
-  <Router>
-    <div>
-      <Nav />
-      <Route exact path="/" component={App} />
-      <Route path="/work" component={WorkList} />
-      <Route path="/blog" component={BlogList} />
-    </div>
-  </Router>
-, document.getElementById('app'));
+render(<Router><App /></Router>, document.getElementById('app'));
