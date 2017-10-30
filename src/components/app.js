51870b0aca43b14babcb82bdd59214f1736dd72a
phil <warnerp18@gmail.com>
2017-10-29 18:38:14 -0700

    initial gallery setup with no styles. adds functionality

--- src/components/app.js
+++ src/components/app.js
@@ -3,21 +3,85 @@ import {
   Route,
   Link,
   Switch
-} from 'react-router-dom'
+} from 'react-router-dom';
+import ReactLoading from 'react-loading';
 
 import HomePage from './home-page';
 import Nav from './nav';
 import WorkList from './work-list';
 import BlogList from './blog-list';
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      hover: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: false,
+    });
+  }
+
+  handleHoverOn(e) {
+    const default = document.querySelector('.default');
+    if(e.target !== test || !default.classList.value.includes('active')) {
+      default.classList.remove('active')
+      e.target.classList.add('active');
+    }
+
+    this.setState({ hover: true});
+  }
+
+  handleHoverOff(e) {
+    e.target.classList.remove('active');
+    this.setState({ hover: false});
+  }
+
   render() {
+    if(this.state.loading) {
+      return <ReactLoading type="bubbles" color="blue" />
+    }
+
+    const defaultActive = document.querySelector('.default');
+    if(!this.state.hover && defaultActive) {
+      defaultActive.classList.add('active'); 
+    }
+
     return (
       <div>
         <Nav />
         <Route path="/work" component={WorkList} />
         <Route path="/blog" component={BlogList} />
+        <div className="imgs">
+          <span className="img"
+            onMouseEnter={e => this.handleHoverOn(e)}
+            onMouseLeave={e => this.handleHoverOff(e)}
+          >Backpacking</span>
+          <span className="img"
+            onMouseEnter={e => this.handleHoverOn(e)}
+            onMouseLeave={e => this.handleHoverOff(e)}
+          >Crossfit</span>
+          <span className="img default active"
+            onMouseEnter={e => this.handleHoverOn(e)}
+            onMouseLeave={e => this.handleHoverOff(e)}
+          >Developer</span>
+          <span className="img"
+            onMouseEnter={e => this.handleHoverOn(e)}
+            onMouseLeave={e => this.handleHoverOff(e)}
+          >Veteran</span>
+          <span className="img"
+            onMouseEnter={e => this.handleHoverOn(e)}
+            onMouseLeave={e => this.handleHoverOff(e)}
+          >Camping</span>
+        </div>
       </div>
     );
   }
 }
+
+export default App; 
